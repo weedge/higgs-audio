59c590a82b4d0814b65b2b06c13b2d2a8fe69883
weedge <weege007@gmail.com>
2025-07-27 22:40:39 +0800

    add generate_delta_stream
    Signed-off-by: weedge <weege007@gmail.com>

--- boson_multimodal/serve/serve_engine.py
+++ boson_multimodal/serve/serve_engine.py
@@ -103,7 +103,6 @@ class AsyncHiggsAudioStreamer(BaseStreamer):
         For text tokens, decodes and caches them until complete words are formed.
         For audio tokens, directly queues them.
         """
-        logger.info(f"{value=}, {value.shape=}, {self.next_tokens_are_prompt=}")
         if value.shape[0] > 1 and not self.next_tokens_are_prompt:
             # This is likely audio tokens (shape: [audio_num_codebooks])
             assert value.shape[0] == self.audio_num_codebooks, "Number of codebooks mismatch"
@@ -207,22 +206,10 @@ class HiggsAudioServeEngine:
         self.device = device
         self.model_name_or_path = model_name_or_path
         self.torch_dtype = torch_dtype
-        if isinstance(torch_dtype, str) and torch_dtype != "auto":
-            self.torch_dtype = getattr(torch, torch_dtype)
-
-        gpu_major = 0
-        if torch.cuda.is_available():
-            gpu_prop = torch.cuda.get_device_properties("cuda")
-            gpu_major = gpu_prop.major
 
         # Initialize model and tokenizer
-        self.model = HiggsAudioModel.from_pretrained(
-            model_name_or_path,
-            torch_dtype=torch_dtype,
-            # `static` cache implementation is not compatible with `attn_implementation==flash_attention_2` make sure to use `sdpa`
-            # attn_implementation="flash_attention_2" if gpu_major >= 8 and self.torch_dtype == torch.bfloat16 else None,
-        ).to(device)
-        logger.info(f"Loaded model from {model_name_or_path}, dtype: {self.model.dtype}, device: {device}")
+        self.model = HiggsAudioModel.from_pretrained(model_name_or_path, torch_dtype=torch_dtype).to(device)
+        logger.info(f"Loaded model from {model_name_or_path}, dtype: {self.model.dtype}")
 
         if tokenizer_name_or_path is None:
             tokenizer_name_or_path = model_name_or_path
@@ -404,16 +391,11 @@ class HiggsAudioServeEngine:
                 ras_win_max_num_repeat=ras_win_max_num_repeat,
                 seed=seed,
             )
-            assert isinstance(outputs, tuple), f"Expected outputs to be a tuple, got {type(outputs)}"
-            logger.info(f"generate text_ids.shape {outputs[0].shape=}")
 
             if len(outputs[1]) > 0:
                 wv_list = []
                 for output_audio in outputs[1]:
-                    logger.info(f"generate audio_ids.shape {output_audio.shape=} {output_audio=}")
-                    # (num_codebooks, seq_len + num_codebooks - 1) -> (num_codebooks, seq_len)
                     vq_code = revert_delay_pattern(output_audio).clip(0, self.audio_codebook_size - 1)[:, 1:-1]
-                    logger.info(f"vq_code shape: {vq_code.shape} {self.audio_codebook_size=} {vq_code=}")
                     wv_numpy = self.audio_tokenizer.decode(vq_code.unsqueeze(0))[0, 0]
                     wv_list.append(wv_numpy)
                 wv_numpy = np.concatenate(wv_list)
@@ -439,3 +421,68 @@ class HiggsAudioServeEngine:
                     "cached_tokens": 0,
                 },
             )
+
+    async def generate_delta_stream(
+        self,
+        chat_ml_sample: ChatMLSample,
+        max_new_tokens: int,
+        temperature: float = 0.7,
+        top_k: Optional[int] = None,
+        top_p: float = 0.95,
+        stop_strings: Optional[List[str]] = None,
+        force_audio_gen: bool = False,
+        ras_win_len: Optional[int] = 7,
+        ras_win_max_num_repeat: int = 2,
+        seed: Optional[int] = None,
+    ):
+        """
+        Generate audio from a chatml sample.
+        Args:
+            chat_ml_sample: A chatml sample.
+            max_new_tokens: The maximum number of new tokens to generate.
+            temperature: The temperature to use for the generation.
+            top_p: The top p to use for the generation.
+            stop_strings: A list of strings to stop the generation.
+            force_audio_gen: Whether to force audio generation. This ensures the model generates audio tokens rather than text tokens.
+            ras_win_len: The length of the RAS window. We use 7 by default. You can disable it by setting it to None or <=0.
+            ras_win_max_num_repeat: The maximum number of times to repeat the RAS window.
+        Returns:
+             Delta AsyncGenerator
+        """
+        # Default stop strings
+        if stop_strings is None:
+            stop_strings = ["<|end_of_text|>", "<|eot_id|>"]
+        if ras_win_len is not None and ras_win_len <= 0:
+            ras_win_len = None
+
+        with torch.no_grad():
+            inputs = self._prepare_inputs(chat_ml_sample, force_audio_gen=force_audio_gen)
+
+            self._prepare_kv_caches()
+
+            streamer = AsyncHiggsAudioStreamer(
+                self.tokenizer,
+                audio_num_codebooks=self.model.config.audio_num_codebooks,
+                skip_prompt=True,
+            )
+            generation_kwargs = dict(
+                **inputs,
+                max_new_tokens=max_new_tokens,
+                use_cache=True,
+                stop_strings=stop_strings,
+                tokenizer=self.tokenizer,
+                do_sample=False if temperature == 0.0 else True,
+                temperature=temperature,
+                top_k=top_k,
+                top_p=top_p,
+                past_key_values_buckets=self.kv_caches,
+                ras_win_len=ras_win_len,
+                ras_win_max_num_repeat=ras_win_max_num_repeat,
+                seed=seed,
+                streamer=streamer,
+            )
+            thread = threading.Thread(target=self.model.generate, kwargs=generation_kwargs)
+            thread.start()
+
+            async for delta in streamer:
+                yield delta
